1819cbe8d03a3ec165d331f6f7447b905c7764bc
fawwazhumam <mfawwazhumam@gmail.com>
2026-03-19 11:18:21 +0700

    [refact] new icon structure with generator

--- packages/icons/tsup.config.ts
+++ packages/icons/tsup.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/react/index.ts'], // Masuk dari file index yang digenerate script tadi
-  format: ['cjs', 'esm'], // Support CommonJS dan ES Modules
-  dts: true, // Generate file definisi TypeScript (.d.ts)
-  minify: true,
+  entry: [
+    "src/index.ts", // Entry utama
+    "src/react/default/outline/index.ts",
+    "src/react/default/solid/index.ts",
+  ],
+  format: ["cjs", "esm"],
+  dts: true,
   clean: true,
-  external: ['react'], // Jangan bundle React-nya, pakai React dari project user
+  external: ["react"],
+  minify: true,
+  // Penting: Agar struktur folder di dist sama dengan di src
+  splitting: false,
+  bundle: true,
 });
